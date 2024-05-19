6605d2c944a67a03c2164f5579211f14918c7b9b
almisraty <almsraty545@gmail.com>
2024-05-19 22:40:32 +0200

    edit seeder

--- database/seeders/EmployeeTableSeeder.php
+++ database/seeders/EmployeeTableSeeder.php
@@ -49,9 +49,10 @@ class EmployeeTableSeeder extends Seeder
         // إنشاء مستخدم
         $user = User::create([
             'email' => "jihad@gmail.com",
-            'password' => Hash::make("1234"),
+            'password' => Hash::make("123456"),
             'id_type_users' => 1,
             'pid' => 1,
+            'id_emp' => 1
         ]);
     }
 }
